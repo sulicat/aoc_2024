62631e4838b5be7b4af157e23fae564954f23e72
Suleiman Barakat <suli.barakat@gmail.com>
2024-12-03 00:19:16 -0500

    day 3

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -2,28 +2,76 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"regexp"
 	"strconv"
+	"strings"
 
 	col "github.com/sulicat/goboi/colors"
-	utils "github.com/sulicat/goboi/utils"
+	"github.com/sulicat/goboi/utils"
+	//utils "github.com/sulicat/goboi/utils"
 )
 
-func main() {
+func p1() {
 	fmt.Printf("%s\n", col.BrightBlue+"Day 3"+col.Reset)
 
-	test_arr := make([]string, 0)
-	test_arr = append(test_arr, "1", "2", "3", "4")
+	file, _ := os.ReadFile("./input.txt")
+	//lines := strings.Split(string(file), "\n")
+	//input := make([][]int, 0)
 
-	int_arr := utils.MapArray(test_arr, func(a string) int {
-		return utils.First(strconv.Atoi(a))
-	})
+	r := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}?\)`)
+	matches := r.FindAllString(string(file), -1)
 
-	utils.ForEach(test_arr, func(a string) {
-		fmt.Printf("asdasd %s\n", a)
-	})
+	sum := 0
+	for _, m := range matches {
+		fmt.Printf("match: %s\n", m)
 
-	for _, val := range int_arr {
-		fmt.Printf("%d\n", val)
+		num_1 := m[4:]
+		num_1 = num_1[:len(num_1)-1]
+
+		vals := strings.Split(num_1, ",")
+		sum += utils.First(strconv.Atoi(vals[0])) * utils.First(strconv.Atoi(vals[1]))
+
 	}
 
+	fmt.Printf("sum: %d\n", sum)
+
+}
+
+func p2() {
+
+	fmt.Printf("%s\n", col.BrightBlue+"Day 3"+col.Reset)
+
+	file, _ := os.ReadFile("./input.txt")
+	//lines := strings.Split(string(file), "\n")
+	//input := make([][]int, 0)
+
+	r := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}?\)|do.*?\(?\)`)
+	matches := r.FindAllString(string(file), -1)
+
+	state := 1
+	sum := 0
+	for _, m := range matches {
+		fmt.Printf("match: %s\n", m)
+
+		if m == "do()" {
+			state = 1
+		} else if m == "don't()" {
+			state = 0
+		} else if state == 1 {
+
+			num_1 := m[4:]
+			num_1 = num_1[:len(num_1)-1]
+
+			vals := strings.Split(num_1, ",")
+			sum += utils.First(strconv.Atoi(vals[0])) * utils.First(strconv.Atoi(vals[1]))
+		}
+	}
+
+	fmt.Printf("sum: %d\n", sum)
+
+}
+
+func main() {
+	p2()
 }
