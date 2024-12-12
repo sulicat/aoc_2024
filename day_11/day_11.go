101379c3c8ff60b5412783330379911edae2d24c
Suleiman Barakat <suli.barakat@gmail.com>
2024-12-11 23:23:50 -0500

    day 11

--- day_11/day_11.go
+++ day_11/day_11.go
@@ -5,13 +5,12 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"sync"
 
 	col "github.com/sulicat/goboi/colors"
 	"github.com/sulicat/goboi/utils"
 )
 
-var data []int64
+var data []int
 
 // 1 -> 0
 // 0 -> 1
@@ -20,11 +19,18 @@ var data []int64
 // 20 48 -> 2 0 4 8
 // 2 0 4 8 ->
 
-func blink(in []int64) []int64 {
-	out := make([]int64, 0)
+type DD struct {
+	Val    int
+	Blinks int
+}
+
+var cache map[DD]int
+
+func blink(in []int) []int {
+	out := make([]int, 0)
 	for _, v := range in {
 
-		nv := int64(0)
+		nv := int(0)
 
 		if v == 0 {
 			nv = 1
@@ -37,8 +43,8 @@ func blink(in []int64) []int64 {
 			if num_digits%2 == 0 {
 				a1 := utils.First(strconv.Atoi(digits[:num_digits/2]))
 				a2 := utils.First(strconv.Atoi(digits[num_digits/2:]))
-				out = append(out, int64(a1))
-				out = append(out, int64(a2))
+				out = append(out, int(a1))
+				out = append(out, int(a2))
 
 			} else {
 				out = append(out, v*2024)
@@ -51,12 +57,21 @@ func blink(in []int64) []int64 {
 
 func count(v int, blinks_left int) int {
 
+	cached, in_cache := cache[DD{v, blinks_left}]
+	if in_cache {
+		//fmt.Printf("chahe use: %d->%d\n", v, cached)
+		return cached
+	}
+
 	if blinks_left == 0 {
 		return 1
 	}
 
 	if v == 0 {
-		return count(1, blinks_left-1)
+		retval := count(1, blinks_left-1)
+		cache[DD{v, blinks_left}] = retval
+		return retval
+
 	} else {
 		digits := strconv.Itoa(int(v))
 		num_digits := len(digits)
@@ -64,9 +79,16 @@ func count(v int, blinks_left int) int {
 		if num_digits%2 == 0 {
 			a1 := utils.First(strconv.Atoi(digits[:num_digits/2]))
 			a2 := utils.First(strconv.Atoi(digits[num_digits/2:]))
-			return count(a1, blinks_left-1) + count(a2, blinks_left-1)
+
+			retval := count(a1, blinks_left-1) + count(a2, blinks_left-1)
+			cache[DD{v, blinks_left}] = retval
+			return retval
+
 		} else {
-			return count(v*2024, blinks_left-1)
+
+			retval := count(v*2024, blinks_left-1)
+			cache[DD{v, blinks_left}] = retval
+			return retval
 		}
 	}
 }
@@ -75,13 +97,11 @@ func p1() {
 	num_blinks := 0
 
 	for {
-		//fmt.Printf("blink: %d ---> %v\n", num_blinks, data)
+		fmt.Printf("blink: %d ---> %v\n", num_blinks, data)
 		if num_blinks >= 10 {
 			fmt.Printf("Num stones: %d\n", len(data))
 			break
 		}
-
-		fmt.Printf("%d %d -> %v\n", num_blinks, len(data), data)
 
 		new_data := blink(data)
 		data = new_data
@@ -93,30 +113,10 @@ func p1() {
 func p2() {
 	num_blinks := 75
 
-	c := make(chan int)
-	var wg sync.WaitGroup
+	total := 0
 
 	for _, d := range data {
-
-		fmt.Printf("d: %v\n", d)
-		wg.Add(1)
-
-		go func(din int, sum chan int, wgin *sync.WaitGroup) {
-			defer wgin.Done()
-			fmt.Printf("go routine: %d\n", d)
-			c <- count(din, num_blinks)
-		}(int(d), c, &wg)
-
-	}
-
-	go func() {
-		wg.Wait()
-		close(c)
-	}()
-
-	total := 0
-	for t := range c {
-		total += t
+		total += count(int(d), num_blinks)
 	}
 
 	fmt.Printf("total p2: %d\n", total)
@@ -128,10 +128,14 @@ func main() {
 	file_data, _ := os.ReadFile("./input.txt")
 	lines := strings.Split(string(file_data), " ")
 
-	data = make([]int64, 0)
+	data = make([]int, 0)
+
+	cache = make(map[DD]int)
+
+	fmt.Printf("cache: %v\n", cache)
 
 	for _, l := range lines {
-		data = append(data, int64(utils.First(strconv.Atoi(l))))
+		data = append(data, int(utils.First(strconv.Atoi(l))))
 	}
 
 	fmt.Printf("%v\n", data)
