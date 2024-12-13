59541ff44a0cbf811e07bcffb0eb56d81a548b91
Suleiman Barakat <suli.barakat@gmail.com>
2024-12-12 22:08:09 -0500

    day 2 part 2

--- day_12/day_12.go
+++ day_12/day_12.go
@@ -12,7 +12,17 @@ import (
 var data [][]string
 var groups []Group
 var group_maps []map[Pos]int
+var group_fences []map[Pos]Fence
+
 var visited map[Pos]int
+
+type Fence struct {
+	Top    bool
+	Right  bool
+	Bottom bool
+	Left   bool
+	P      Pos
+}
 
 type Pos struct {
 	R int
@@ -119,6 +129,103 @@ func count_fences(p Pos, groupmap map[Pos]int) int {
 	return out
 }
 
+func update_fences(p Pos, groupmap map[Pos]int, fencemap *map[Pos]Fence) int {
+	out := 4
+	dirs := []Pos{
+		{p.R - 1, p.C},
+		{p.R + 1, p.C},
+		{p.R, p.C - 1},
+		{p.R, p.C + 1},
+	}
+
+	new_fence := Fence{}
+	new_fence.P = p
+
+	for di, d := range dirs {
+		_, inmap := groupmap[d]
+		if inmap {
+			out -= 1
+		} else {
+
+			switch di {
+			case 0:
+				new_fence.Top = true
+			case 1:
+				new_fence.Bottom = true
+			case 2:
+				new_fence.Left = true
+			case 3:
+				new_fence.Right = true
+			}
+
+		}
+
+	}
+
+	(*fencemap)[p] = new_fence
+
+	return out
+}
+
+func count_island(in map[Pos]bool) int {
+	out := 0
+
+	rvis := map[Pos]int{}
+
+	var dfs func(p Pos)
+	dfs = func(p Pos) {
+
+		rvis[p] = 1
+		adj := get_adj(p)
+		for _, a := range adj {
+
+			_, already_visited := rvis[a]
+			_, exists := in[a]
+			if exists &&
+				!already_visited && in[a] {
+
+				dfs(a)
+			}
+		}
+	}
+
+	for pos := range in {
+		_, iv := rvis[pos]
+		if !iv && in[pos] {
+			dfs(pos)
+			out += 1
+		}
+
+	}
+
+	return out
+}
+
+func get_edges(gi int) int {
+	// we will dfs to find all bot, top, left and right fences
+
+	// dfs_visited := map[Pos]int{}
+
+	top_fences := map[Pos]bool{}
+	bot_fences := map[Pos]bool{}
+	left_fences := map[Pos]bool{}
+	right_fences := map[Pos]bool{}
+
+	for _, p := range group_fences[gi] {
+		top_fences[p.P] = p.Top
+		bot_fences[p.P] = p.Bottom
+		left_fences[p.P] = p.Left
+		right_fences[p.P] = p.Right
+	}
+
+	top_count := count_island(top_fences)
+	left_count := count_island(left_fences)
+	right_count := count_island(right_fences)
+	bot_count := count_island(bot_fences)
+
+	return top_count + left_count + right_count + bot_count
+}
+
 func p1() {
 
 	out := 0
@@ -155,6 +262,51 @@ func p1() {
 	}
 
 	fmt.Printf("TOTAL p1: %d\n", out)
+}
+
+func p2() {
+
+	out := 0
+
+	for r := range data {
+		for c := range data[r] {
+
+			_, is_visited := visited[Pos{r, c}]
+			if !is_visited {
+				cells := recurse_links(Pos{r, c})
+
+				groups = append(groups, cells)
+				group_map := make(map[Pos]int)
+				for _, cell := range cells {
+					group_map[Pos{cell.R, cell.C}] = 1
+				}
+				group_maps = append(group_maps, group_map)
+
+				group_fence := make(map[Pos]Fence)
+				group_fences = append(group_fences, group_fence)
+			}
+		}
+	}
+
+	for gi, g := range groups {
+		fmt.Printf("Group: %d %s len: %d\n", gi, data[g[0].R][g[0].C], len(g))
+		fmt.Printf("map: %v\n ", group_maps[gi])
+
+		sum_fences := 0
+		for _, p := range g {
+			sum_fences += update_fences(p, group_maps[gi], &group_fences[gi])
+		}
+		// out += sum_fences * len(g)
+		// fmt.Printf("%d * %d = %d", len(g), sum_fences, sum_fences*len(g))
+		// fmt.Printf("\n")
+
+		edges := get_edges(gi)
+		fmt.Printf("EDGES %d => %d\n", edges, edges*len(g))
+		fmt.Printf("\n")
+		out += edges * len(g)
+	}
+
+	fmt.Printf("total p2: %d\n", out)
 
 }
 
@@ -173,12 +325,13 @@ func main() {
 		}
 	}
 
-	fmt.Printf("%v\n", data)
+	// fmt.Printf("%v\n", data)
 
 	visited = make(map[Pos]int)
 
 	group_maps = make([]map[Pos]int, 0)
+	group_fences = make([]map[Pos]Fence, 0)
 
-	p1()
-
+	// p1()
+	p2()
 }
